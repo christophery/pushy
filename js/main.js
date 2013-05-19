a6eec1a2a5c48c56a45c589603c972a58b4966b1
Christopher Yee <christophery@gmail.com>
2013-05-19 19:41:32 -0400

    using 3d transforms

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ $("button").click(function () {
 	$('.pushy').toggleClass("pushy-menu-open");
 	$('#container').toggleClass("container-push");
 });
+
+$("nav a").click(function () {
+	$('.pushy').toggleClass("pushy-menu-left");
+	$('.pushy').toggleClass("pushy-menu-open");
+	$('#container').toggleClass("container-push");
+});
