1e32e19471b057776255d6bfb21a18dab716b155
Christopher Yee <christophery@gmail.com>
2013-06-01 19:31:32 -0400

    Fixed performance issue with mobile devices

--- README.md
+++ README.md
@@ -1,37 +1,50 @@
 #Pushy
 
-Pushy is an off-canvas navigation menu for your website.
+Pushy is an responsive off-canvas navigation menu, using CSS transforms & transitions.
+
+Feel free to [let me know](http://www.twitter.com/cmyee) if you use Pushy in one of your websites.
 
 [View Demo](http://www.christopheryee.ca/pushy) |
 [View Project on GitHub](https://github.com/christophery/pushy/)
 
 ##Features
 
-- Uses CSS 3D transforms for better performance on mobile.
-- Fallback to jQuery (IE < 9).
+- Uses CSS transforms & transitions.
+- Smooth performance on mobile devices.
+- jQuery animation fallback for IE 7 - 9.
 - Menu closes when link is clicked.
 
 ##Requirements
 
-- jQuery 1.9.1
+- jQuery 1.10
 - Modernizr
 
 ##Browser Compatibility
 
 Desktop
-- IE 7+
+
+- IE 7-10
 - Chrome
 - Firefox
 - Safari (Mac)
 
 Mobile
+
 - Chrome (Android 4.2.2)
 - Android Browser (Android 4.2.2)
 - Safari (iOS 6)
 
-##What I Used
+##Version History
 
-- HTML5 Boilerplate
-- jQuery
-- CSS3
-- Modernizr
+0.8.4
+
+- Fixed performance issue with mobile devices
+- Updated to jQuery 1.10
+- Updated the demo file
+- Updated the read me
+
+##Thanks to
+
+- [HTML5 Boilerplate](http://html5boilerplate.com/)
+- [jQuery](http://jquery.com/)
+- [Modernizr](http://modernizr.com/)
--- js/pushy.js
+++ js/pushy.js
@@ -1,41 +1,45 @@
-/*! Pushy - v0.8.3 - 2013-5-24
+/*! Pushy - v0.8.4 - 2013-6-1
 * Pushy is an off-canvas navigation menu for your website.
 * https://github.com/christophery/pushy/
 * by Christopher Yee */
 
-var pushy = $('.pushy'), //menu css class
-	container = $('#container'), //container css class
-	pushyClass = "pushy-left pushy-open", //menu position & menu open class
-	containerClass = "container-push", //container open class
-	menuBtn = $('.menu-btn, .pushy a'), //css classes to toggle the menu
-	menuSpeed = 200, //jQuery fallback menu speed
-	menuWidth = pushy.width() + "px"; //jQuery fallback menu width
+$(function() {
+	var pushy = $('.pushy'), //menu css class
+		container = $('#container'), //container css class
+		pushyClass = "pushy-left pushy-open", //menu position & menu open class
+		containerClass = "container-push", //container open class
+		menuBtn = $('.menu-btn, .pushy a'), //css classes to toggle the menu
+		menuSpeed = 200, //jQuery fallback menu speed
+		menuWidth = pushy.width() + "px"; //jQuery fallback menu width
 
-if(!Modernizr.csstransforms3d){
-	//jQuery fallback (no 3D transforms support)
-	pushy.css({left: "-" + menuWidth}); //hide menu by default
-	container.css({"overflow-x": "hidden"}); //fixes IE issue
+	if(!Modernizr.csstransforms3d){
+		//jQuery fallback (no 3D transforms support)
+		pushy.css({left: "-" + menuWidth}); //hide menu by default
+		container.css({"overflow-x": "hidden"}); //fixes IE issue
 
-	//keep track of menu state (open/close)
-	var state = true;
+		//keep track of menu state (open/close)
+		var state = true;
 
-	//toggle menu
-	menuBtn.click(function() {
-		if (state) {
-			pushy.animate({left: "0px"}, menuSpeed);
-			container.animate({left: menuWidth}, menuSpeed);
-			state = false;
-		} else {
-			pushy.animate({left: "-" + menuWidth}, menuSpeed);
-			container.animate({left: "0px"}, menuSpeed);
-			state = true;
-		}
-	});
+		//toggle menu
+		menuBtn.click(function() {
+			if (state) {
+				pushy.animate({left: "0px"}, menuSpeed);
+				container.animate({left: menuWidth}, menuSpeed);
+				state = false;
+			} else {
+				pushy.animate({left: "-" + menuWidth}, menuSpeed);
+				container.animate({left: "0px"}, menuSpeed);
+				state = true;
+			}
+			e.preventDefault();
+		});
 
-}else{
-	//use native CSS 3D Transforms
-	menuBtn.click(function () {
-		pushy.toggleClass(pushyClass);
-		container.toggleClass(containerClass);
-	});
-}
+	}else{
+		//use native CSS 3D Transforms
+		menuBtn.click(function(e) {
+			pushy.toggleClass(pushyClass);
+			container.toggleClass(containerClass);
+			e.preventDefault();
+		});
+	}
+});
