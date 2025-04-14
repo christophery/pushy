25237a96f3d79e00795105b2996d982ad76fc0f5
Chris Yee <christophery@gmail.com>
2025-04-14 15:31:20 -0400

    added watch back

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,29 +2,49 @@ module.exports = function (grunt) {
     const sass = require('sass');
 
     grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-        options: {
-            implementation: sass,
-            sourceMap: true
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+          options: {
+            livereload: true,
+            spawn: false,
+          },
+          css: {
+            files: ['scss/*.scss'],
+            tasks: ['sass-task'],
+          },
+          js: {
+            files: ['js/*.js'],
+            tasks: ['js-task'],
+          },
         },
-        dist: {
-            files: {
-            'css/pushy.css': 'scss/pushy.scss',
-            'css/demo.css': 'scss/demo.scss',
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                'css/pushy.css': 'scss/pushy.scss',
+                'css/demo.css': 'scss/demo.scss',
+                }
             }
-        }
-    },
-    uglify: {
-        dist: {
-            files: {
-            'js/pushy.min.js': 'js/pushy.js'
+        },
+        uglify: {
+            dist: {
+                files: {
+                'js/pushy.min.js': 'js/pushy.js'
+                }
             }
-        }
-    },
+        },
     });
-  
+
+    // Load grunt plugins
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');  
-    grunt.registerTask('default', ['sass', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('js-task', ['uglify']);
+    grunt.registerTask('sass-task', ['sass']);
 };  
